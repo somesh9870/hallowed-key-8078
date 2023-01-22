0b9cdc12b23c285698c9c49999b95477561eddc9
somesh9870 <someshrawat106@gmail.com>
2023-01-22 08:15:53 +0530

    day-05 done

---
+++ style-incore/src/Pages/ProductDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <>
+       <Heading>Olive Brown Extra Slim Sateen Stretch Suit</Heading>
+       <Flex>
+       {/* <Image src={} alt={} /> */}
+        </Flex> 
+    </>
+  )
+}
+
+export default ProductDetails
